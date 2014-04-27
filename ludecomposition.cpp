4f5a3c68816d7fb667b588e0af99cb998d7da9e6
sreezin <abdul.kader880@gmail.com>
2014-04-27 01:13:11 -0600

    sequential done

--- ludecomposition.cpp
+++ ludecomposition.cpp
@@ -27,11 +27,11 @@ class matrix{
 			for(int j=0;j<col;j++)
 				mat[i][j]=super[ri+i][cj+j];	
 	}
-	void set(int super[][3],int ri,int cj){//after creating matrix need to set values;
+	void set(double linear[]){//after creating matrix need to set values;
 	
 		for(int i=0;i<row;i++)
 			for(int j=0;j<col;j++)
-				mat[i][j]=super[ri+i][cj+j];	
+				mat[i][j]=linear[i*col+j];	
 	}
 	void mapSub(matrix LU99,int ri,int cj){
 		
@@ -91,18 +91,158 @@ matrix subtract(matrix A, matrix B ){
 		
 }
 
+matrix combineMatrix(matrix A00,matrix A01,matrix A10,matrix A11){
+
+	matrix M(A00.row+A10.row,A00.col+A01.col);
+
+	M.mapSub(A00,0,0);
+	M.mapSub(A01,0,A00.col);
+	M.mapSub(A10,A00.row,0);
+	M.mapSub(A11,A00.row,A00.col);
+	
+	return M;
+
+}
+
+/*
 matrix solveLower(matrix A, matrix L){
 	
 	
+	int r=A.row;
+	int c=A.col;
 	
-	return U;
+	int r1=r/2; 
+	int r2=r-r1;
+	int c1=c/2;
+	int c2=c-c1;
+	
+	matrix A00(r1,c1),A01(r1,c2),A10(r2,c1),A11(r2,c2);
+	A00.set(A.mat,0,0);
+	A01.set(A.mat,0,c1);
+	A10.set(A.mat,r1,0);
+	A11.set(A.mat,r1,c1);
+	
+	matrix L00(r1,c1),L10(r2,c1),L11(r2,c2);
+	L00.set(L.mat,0,0);
+	L10.set(L.mat,r1,0);
+	L11.set(L.mat,r1,c1);
+	
+	matrix U00=solveLower(A00,L00);
+	matrix U01=solveLower(A01,L00);
+	
+	matrix iA10=subtract(A10,multiply(L10,U00));
+	matrix U10=solveLower(iA10,L11); 
+	
+	matrix iA11=subtract(A11,multiply(L10,U01));
+	matrix U11=solveLower(iA11,L11); 
+	
+	
+	return combineMatrix(U00,U01,U10,U11);
 }
 
+matrix solveUpper(matrix A, matrix U){//wrong
+	
+	int r=A.row;
+	int c=A.col;
+	
+	int r1=r/2; 
+	int r2=r-r1;
+	int c1=c/2;
+	int c2=c-c1;
+	
+	matrix A00(r1,c1),A01(r1,c2),A10(r2,c1),A11(r2,c2);
+	A00.set(A.mat,0,0);
+	A01.set(A.mat,0,c1);
+	A10.set(A.mat,r1,0);
+	A11.set(A.mat,r1,c1);
+	
+	matrix U00(r1,c1),U01(r1,c2),U11(r2,c2);
+	U00.set(U.mat,0,0);
+	U01.set(U.mat,0,c1);
+	U11.set(U.mat,r1,c1);
+	
+	matrix L00=solveUpper(A00,U00);
+	matrix L10=solveUpper(A10,U00);
+	
+	matrix iA01=subtract(A01,multiply(L00,U01));
+	matrix L01=solveUpper(iA01,U11); 
+	
+	matrix iA11=subtract(A11,multiply(L10,U01));
+	matrix L11=solveUpper(iA11,U11); 
+	
+	return combineMatrix(L00,L01,L10,L11);
+}
+*/
 matrix solveUpper(matrix A, matrix U){
+	if(A.row==0|| A.col==0){
+		matrix zero(0,0);
+		return zero;
+	}
+	else if(U.col==1){
+	
+		for(int i=0;i<A.row;i++)
+			A.mat[i][0]/=U.mat[0][0];
+		return A;
+	}
+	
+	matrix L00(1,1);
+	L00.mat[0][0]=A.mat[0][0]/U.mat[0][0];
+	
+	matrix A01(1,A.col-1),A10(A.row-1,1),A11(A.row-1,A.col-1);
+	A01.set(A.mat,0,1);
+	A10.set(A.mat,1,0);
+	A11.set(A.mat,1,1);
+	
+	matrix U01(1,U.col-1),U11(U.row-1,U.col-1);
+	U01.set(U.mat,0,1);
+	U11.set(U.mat,1,1);
+	
+	matrix L10(A10.row,A10.col);
+	for(int i=0;i<L10.row;i++)
+		L10.mat[i][0]=A10.mat[i][0]/U.mat[0][0];
+		
+	matrix iA01=subtract(A01,multiply(L00,U01));
+	matrix L01=solveUpper(iA01,U11);
+
+	
+	matrix iA11=subtract(A11,multiply(L10,U01));
+	matrix L11=solveUpper(iA11,U11);
+	
+	return combineMatrix(L00,L01,L10,L11);
+}
+matrix solveLower(matrix A, matrix L){
+	
+	if(A.row==0 || A.col==0){
+	
+		matrix zero(0,0);
+		return zero;
+	}
+	else if(L.row==1){
+		return A;
+	}
 	
 	
+	matrix U00(1,1);
+	U00.set(A.mat,0,0);
 	
-	return L;
+	matrix U01(1,A.col-1);
+	U01.set(A.mat,0,1);
+	
+	matrix A10(A.row-1,1),A11(A.row-1,A.col-1);
+	A10.set(A.mat,1,0);
+	A11.set(A.mat,1,1);
+	
+	matrix L10(L.row-1,1),L11(L.row-1,L.col-1);
+	L10.set(L.mat,1,0);
+	L11.set(L.mat,1,1);
+	
+	matrix iA10=subtract(A10,multiply(L10,U00));
+	matrix U10=solveLower(iA10,L11);
+	
+	matrix iA11=subtract(A11,multiply(L10,U01));
+	matrix U11=solveLower(iA11,L11);
+	
+	return combineMatrix(U00,U01,U10,U11);
 }
 matrix makeL(matrix L00,matrix L10,matrix L11){
 
@@ -133,7 +273,7 @@ matrix makeU(matrix U00,matrix U01,matrix U11){
 	
 	return U;
 }
-pair<matrix,matrix> LUDecompose(A){
+pair<matrix,matrix> LUDecompose(matrix A){
 
 
 	if(A.row==1){
@@ -185,7 +325,7 @@ pair<matrix,matrix> LUDecompose(A){
 	matrix U01=solveLower(A01,L00);
 	matrix L10=solveUpper(A10,U00);
 	
-	matrix iA11=sumtract(A11,multiply(L10,U01));
+	matrix iA11=subtract(A11,multiply(L10,U01));
 	
 	pair<matrix,matrix> q=LUDecompose(iA11);
 	matrix L11=q.first;
@@ -197,13 +337,43 @@ pair<matrix,matrix> LUDecompose(A){
 	return pair<matrix,matrix>(L,U);
 	
 }
+
 int main(){
 	
 	
-	int ar[3][3]={{1,-2,3},{2,-5,12},{0,2,-10}};
-	matrix A(3,3);
-	A.set(ar,0,0);
-	A.show();
+	double ar[]={0.81472,0.09754,0.15761,0.14189,0.65574,
+0.90579,0.2785,0.97059,0.42176,0.035712,
+0.12699,0.54688,0.95717,0.91574,0.84913,
+0.91338,0.95751,0.48538,0.79221,0.93399,
+0.63236,0.96489,0.80028,0.95949,0.67874};
+	matrix AA(5,5);
+	AA.set(ar);
+	pair<matrix, matrix> p=LUDecompose(AA);
+	p.first.show();
+	p.second.show();
 	
+	multiply(p.first,p.second).show();
+	/*
+	int l[]={1,0,0,	3,1,0,	4,5,1};
+	matrix L(3,3);
+	L.set(l);
+	
+	int a[]={5,	6,	7};
+	matrix A(3,1);
+	A.set(a);
+	
+	solveLower(A,L).show();
+	
+	int u[]={1,5,7,	0,6,8,	0,0,9};
+	matrix U(3,3);
+	U.set(u);
+	
+	matrix B(1,3);
+	B.set(a);
+	matrix X=solveUpper(B,U);
+	X.show();	
+	multiply(X,U).show();
+*/	
+
 return 0;
 }
