7ec3360653d51469292142b1329eaeff5db4cb70
sreezin <abdul.kader880@gmail.com>
2014-04-28 21:40:45 -0600

    all done

--- OMP-LUDecomposition.cpp
+++ OMP-LUDecomposition.cpp
@@ -1,3 +1,4 @@
+
 /*
 Author: Md Abdul Kader
 University Of Texas at El Paso
@@ -6,12 +7,10 @@ University Of Texas at El Paso
 #include<cmath>
 #include<cstdio>
 #include<iostream>
-#include<vector>
+
 #include<string>
-#include<cstring>
 using namespace std;
-typedef vector<double> vi;
-typedef vector<vector<double> >vii;
+
 class matrix{
 	public:
 	int row,col;
@@ -26,42 +25,12 @@ class matrix{
 		}
 	}
 	matrix(){row=col=0;mat=NULL;}
-	/*
-	matrix(const matrix& obj){
-    	row=obj.row;
-		col=obj.col;
-		mat=new double*[row];
-		for(int i=0;i<row;i++){
-			mat[i] = new double[col];
-		}
-    	for(int i=0;i<row;i++)
-    		memcpy( &mat[i], &obj.mat[i], col );
-	}
-	*/
-	void clear(){
-		
-			 for(int i = 0; i < row; ++i) {
-				 delete [] mat[i];
-			 }
-			 delete [] mat;
-		
-	}
-	/*
-	matrix& operator= (const matrix& other){
-    	if (this != &other) {
-        	for(int i=0;i<row;i++)
-    			memcpy( &mat[i], &other.mat[i], col );
-    	}
-    	return *this;
-	}
-	*/
 	void set(matrix super,int ri,int cj){
 	
 		for(int i=0;i<row;i++)
 			for(int j=0;j<col;j++)
 				mat[i][j]=super.mat[ri+i][cj+j];	
 	}
-	
 	void set(double linear[]){
 	
 		for(int i=0;i<row;i++)
@@ -98,18 +67,21 @@ matrix multiply(matrix A, matrix B ){
 	int INNER=A.col;
 	
 	
-	for (int row = 0; row != ROW; ++row) 
-	{
- 		for (int col = 0; col != COL; ++col)
+		#pragma omp parallel for	
+		for (int row = 0; row < ROW; ++row) 
 		{
-			double sum = 0;
-			for (int inner = 0; inner != INNER; ++inner)
+			for (int col = 0; col < COL; ++col)
 			{
-				sum += A.mat[row][inner] * B.mat[inner][col];
+				double sum = 0;
+				for (int inner = 0; inner < INNER; ++inner)
+				{
+					sum += A.mat[row][inner] * B.mat[inner][col];
+				}
+				C.mat[row][col] = sum;
 			}
-			C.mat[row][col] = sum;
 		}
-	}
+		
+	
 	
 	return C;
 }
@@ -139,6 +111,113 @@ matrix combineMatrix(matrix A00,matrix A01,matrix A10,matrix A11){
 
 }
 
+/*
+matrix solveUpper(matrix A, matrix U){
+	if(A.row==0|| A.col==0){
+		matrix zero(0,0);
+		return zero;
+	}
+	else if(U.col==1){
+	
+		for(int i=0;i<A.row;i++)
+			A.mat[i][0]/=U.mat[0][0];
+		return A;
+	}
+	
+	matrix L00(1,1);
+	L00.mat[0][0]=A.mat[0][0]/U.mat[0][0];
+	
+	matrix A01(1,A.col-1),A10(A.row-1,1),A11(A.row-1,A.col-1);
+	A01.set(A,0,1);
+	A10.set(A,1,0);
+	A11.set(A,1,1);
+	
+	matrix U01(1,U.col-1),U11(U.row-1,U.col-1);
+	U01.set(U,0,1);
+	U11.set(U,1,1);
+	
+	matrix L10(A10.row,A10.col);
+	for(int i=0;i<L10.row;i++)
+		L10.mat[i][0]=A10.mat[i][0]/U.mat[0][0];
+		
+	matrix L11,L01;
+	if(A.row>64){
+		#pragma omp task shared(L01)
+   		{ 
+   			matrix iA01=subtract(A01,multiply(L00,U01));
+			L01=solveUpper(iA01,U11);
+
+   		}
+		#pragma omp task shared(L11)
+   		{ 
+   			matrix iA11=subtract(A11,multiply(L10,U01));
+			L11=solveUpper(iA11,U11);
+   		}
+
+		#pragma omp taskwait
+	}
+	else {
+			matrix iA01=subtract(A01,multiply(L00,U01));
+			L01=solveUpper(iA01,U11);
+
+			matrix iA11=subtract(A11,multiply(L10,U01));
+			L11=solveUpper(iA11,U11);
+	}
+	
+	return combineMatrix(L00,L01,L10,L11);
+}
+matrix solveLower(matrix A, matrix L){
+	
+	if(A.row==0 || A.col==0){
+	
+		matrix zero(0,0);
+		return zero;
+	}
+	else if(L.row==1){
+		return A;
+	}
+	
+	
+	matrix U00(1,1);
+	U00.set(A,0,0);
+	
+	matrix U01(1,A.col-1);
+	U01.set(A,0,1);
+	
+	matrix A10(A.row-1,1),A11(A.row-1,A.col-1);
+	A10.set(A,1,0);
+	A11.set(A,1,1);
+	
+	matrix L10(L.row-1,1),L11(L.row-1,L.col-1);
+	L10.set(L,1,0);
+	L11.set(L,1,1);
+	
+	matrix  U10,U11;
+	if(A.row>64){
+		#pragma omp task shared(U10)
+   		{ 
+   			matrix iA10=subtract(A10,multiply(L10,U00));
+			U10=solveLower(iA10,L11);
+   		}
+   		#pragma omp task shared(U11)
+   		{ 
+   			matrix iA11=subtract(A11,multiply(L10,U01));
+			 U11=solveLower(iA11,L11);
+   		}
+		#pragma omp taskwait
+	}
+	else {
+			matrix iA10=subtract(A10,multiply(L10,U00));
+			U10=solveLower(iA10,L11);
+			matrix iA11=subtract(A11,multiply(L10,U01));
+			U11=solveLower(iA11,L11);
+	}
+	
+	
+	
+	return combineMatrix(U00,U01,U10,U11);
+}
+*/
 matrix solveUpper1(matrix A, matrix U){
 	if(A.row==0|| A.col==0){
 		matrix zero(0,0);
@@ -236,15 +315,25 @@ matrix solveLower(matrix A, matrix L){ //div/2 algo
 	L10.set(L,r1,0);
 	L11.set(L,r1,cL1);
 	
-	matrix U00=solveLower(A00,L00);
-	matrix U01=solveLower(A01,L00);
+	matrix U00,U01,U10,U11,iA10,iA11;
 	
-	matrix iA10=subtract(A10,multiply(L10,U00));
-	matrix U10=solveLower(iA10,L11); 
+	#pragma omp task shared(U00)
+	U00=solveLower(A00,L00);
+	#pragma omp task shared(U01)
+	U01=solveLower(A01,L00);
+	#pragma omp taskwait
 	
-	matrix iA11=subtract(A11,multiply(L10,U01));
-	matrix U11=solveLower(iA11,L11); 
-	
+	#pragma omp task shared(iA10)
+	 iA10=subtract(A10,multiply(L10,U00));
+	 #pragma omp task shared(iA11)
+	 iA11=subtract(A11,multiply(L10,U01));
+	 #pragma omp taskwait
+	 
+	 #pragma omp task shared(U10)
+	 U10=solveLower(iA10,L11); 
+	 #pragma omp task shared(U11)
+ 	 U11=solveLower(iA11,L11); 
+	#pragma omp taskwait
 	
 	return combineMatrix(U00,U01,U10,U11);
 }
@@ -274,19 +363,27 @@ matrix solveUpper(matrix A, matrix U){// div/2 algo
 	U01.set(U,0,c1);
 	U11.set(U,rU1,c1);
 	
-	matrix L00=solveUpper(A00,U00);
-	matrix L10=solveUpper(A10,U00);
+	matrix L10,L01,L11,L00,iA01,iA11;
+	#pragma omp task shared(L00)
+	{L00=solveUpper(A00,U00);}
+	#pragma omp task shared(L10)
+	{L10=solveUpper(A10,U00);}
+	#pragma omp taskwait
 	
-	matrix iA01=subtract(A01,multiply(L00,U01));
-	matrix L01=solveUpper(iA01,U11); 
+	#pragma omp task shared(iA01)
+	{	iA01=subtract(A01,multiply(L00,U01));}
+	#pragma omp task shared(iA11)
+	{	iA11=subtract(A11,multiply(L10,U01));}
+	#pragma omp taskwait
 	
-	matrix iA11=subtract(A11,multiply(L10,U01));
-	matrix L11=solveUpper(iA11,U11); 
+	#pragma omp task shared(L01)
+	L01=solveUpper(iA01,U11); 
+	#pragma omp task shared(L11)
+	L11=solveUpper(iA11,U11); 
+	#pragma omp taskwait
 	
 	return combineMatrix(L00,L01,L10,L11);
 }
-
-
 matrix makeL(matrix L00,matrix L10,matrix L11){
 
 	matrix L(L00.row+L10.row,L00.col+L11.col);
@@ -365,11 +462,26 @@ pair<matrix,matrix> LUDecompose(matrix A){
 	matrix L00=p.first;
 	matrix U00=p.second;
 	
-	matrix U01=solveLower(A01,L00);
-	matrix L10=solveUpper(A10,U00);
+	matrix U01,L10;
+	if(A.row>64){
+		#pragma omp task shared(U01)
+   		{ 
+   			U01=solveLower(A01,L00);
+   		}
+   		#pragma omp task shared(L10)
+   		{ 
+   			L10=solveUpper(A10,U00);
+   		}
+		#pragma omp taskwait
+	}
+	else {
+	
+		U01=solveLower(A01,L00);
+		L10=solveUpper(A10,U00);
+	}
+	
 	
 	matrix iA11=subtract(A11,multiply(L10,U01));
-	
 	pair<matrix,matrix> q=LUDecompose(iA11);
 	matrix L11=q.first;
 	matrix U11=q.second;
@@ -401,25 +513,31 @@ int main(){
 	
 	const int dim=2500;
 	
-	double *ar;
-	ar=new double[dim*dim];
+	double ar[dim*dim];
 	getMatrix(dim,ar);
 	
 	matrix AA(dim,dim);
 	AA.set(ar);
-	delete ar;
-	double st=omp_get_wtime();
-	pair<matrix, matrix> p=LUDecompose(AA);
-	double et=omp_get_wtime()-st;
-	printf("Sequential Elapsed time: %lf\n",et);
+	pair<matrix, matrix> p;
 	
+	omp_set_dynamic(1);
+  	omp_set_num_threads(2);
+	
+	double start_t=omp_get_wtime();
+	#pragma omp parallel shared(AA)
+	{
+		#pragma omp single
+		{
+			p=LUDecompose(AA);
+		
+		}
+		
+	}
+	double elapse_t=omp_get_wtime()-start_t;
+	
+	printf("Omp Elapsed time: %lf\n",elapse_t);
 	cout<<"Is it correct solution? "<<isEqual(AA,multiply(p.first,p.second))<<endl;
-	/*
-	p.first.show();
-	p.second.show();
-	*/
-	
-	
+
 	
 
 return 0;
--- cilkLUDecomposition.cpp
+++ cilkLUDecomposition.cpp
@@ -7,55 +7,31 @@ University Of Texas at El Paso
 #include<cmath>
 #include<cstdio>
 #include<iostream>
-#include<vector>
+#include <cilk/cilk_api.h>
 #include <cilk/cilk.h>
 using namespace std;
-typedef vector<double> vi;
-typedef vector<vector<double> >vii;
+
 class matrix{
 	public:
 	int row,col;
-	vector<vi> mat;
+	double **mat;
 	matrix(int r,int c){
 		
 		row=r;
 		col=c;
-		for(int i=0;i<r;i++){
-			vi t(c);
-			mat.push_back(t);
-		}
-		
-	}
-	matrix(){
-		//printf("empty constructor\n");
-	}
-	/*
-	matrix(const matrix &obj){
-		
-		//printf("copy constructor s\n");
-		row=obj.row;
-		col=obj.col;
-		
-		//mat=obj.mat;
-		
+		mat=new double*[row];
 		for(int i=0;i<row;i++){
-			vi t(col);
-			mat.push_back(t);
+			mat[i] = new double[col];
 		}
-		for(int i=0;i<row;i++)
-			for(int j=0;j<col;j++)
-				mat[i][j]=obj.mat[i][j];
-				
-		//printf("copy constructor id %d\n",omp_get_thread_num());
 	}
-	*/
-	void set(vii super,int ri,int cj){//after creating matrix need to set values;
+	matrix(){row=col=0;mat=NULL;}
+	void set(matrix super,int ri,int cj){
 	
 		for(int i=0;i<row;i++)
 			for(int j=0;j<col;j++)
-				mat[i][j]=super[ri+i][cj+j];	
+				mat[i][j]=super.mat[ri+i][cj+j];	
 	}
-	void set(double linear[]){//after creating matrix need to set values;
+	void set(double linear[]){
 	
 		for(int i=0;i<row;i++)
 			for(int j=0;j<col;j++)
@@ -83,6 +59,7 @@ class matrix{
 
 };
 
+
 matrix multiply(matrix A, matrix B ){
 
 	matrix C(A.row,B.col);
@@ -91,12 +68,12 @@ matrix multiply(matrix A, matrix B ){
 	int INNER=A.col;
 	
 	
-	cilk_for (int row = 0; row != ROW; ++row) 
+	cilk_for (int row = 0; row < ROW; ++row) 
 	{
- 		for (int col = 0; col != COL; ++col)
+ 		for (int col = 0; col < COL; ++col)
 		{
 			double sum = 0;
-			for (int inner = 0; inner != INNER; ++inner)
+			for (int inner = 0; inner < INNER; ++inner)
 			{
 				sum += A.mat[row][inner] * B.mat[inner][col];
 			}
@@ -111,7 +88,7 @@ matrix subtract(matrix A, matrix B ){
 
 	matrix C(A.row,A.col);
 	
-	cilk_for(int i=0;i<A.row;i++)
+	for(int i=0;i<A.row;i++)
 		for(int j=0;j<A.col;j++)
 			 C.mat[i][j]=A.mat[i][j]-B.mat[i][j];
 	return C;
@@ -123,19 +100,17 @@ matrix combineMatrix(matrix A00,matrix A01,matrix A10,matrix A11){
 
 	matrix M(A00.row+A10.row,A00.col+A01.col);
 
-	cilk_spawn M.mapSub(A00,0,0);
-	cilk_spawn M.mapSub(A01,0,A00.col);
-	cilk_spawn M.mapSub(A10,A00.row,0);
-	cilk_spawn M.mapSub(A11,A00.row,A00.col);
+	 M.mapSub(A00,0,0);
+	 M.mapSub(A01,0,A00.col);
+	 M.mapSub(A10,A00.row,0);
+	 M.mapSub(A11,A00.row,A00.col);
 	
-	cilk_sync;
+	
 	return M;
 
 }
 
-matrix solveUpper(matrix A, matrix U){
-	
-	//printf("Upper ID %d\n",omp_get_thread_num());
+matrix solveUpper1(matrix A, matrix U){
 	if(A.row==0|| A.col==0){
 		matrix zero(0,0);
 		return zero;
@@ -151,33 +126,28 @@ matrix solveUpper(matrix A, matrix U){
 	L00.mat[0][0]=A.mat[0][0]/U.mat[0][0];
 	
 	matrix A01(1,A.col-1),A10(A.row-1,1),A11(A.row-1,A.col-1);
-	A01.set(A.mat,0,1);
-	A10.set(A.mat,1,0);
-	A11.set(A.mat,1,1);
+	A01.set(A,0,1);
+	A10.set(A,1,0);
+	A11.set(A,1,1);
 	
 	matrix U01(1,U.col-1),U11(U.row-1,U.col-1);
-	U01.set(U.mat,0,1);
-	U11.set(U.mat,1,1);
+	U01.set(U,0,1);
+	U11.set(U,1,1);
 	
 	matrix L10(A10.row,A10.col);
-	cilk_for(int i=0;i<L10.row;i++)
+	for(int i=0;i<L10.row;i++)
 		L10.mat[i][0]=A10.mat[i][0]/U.mat[0][0];
 		
-	matrix L11,L01;
+	matrix iA01=subtract(A01,multiply(L00,U01));
+	matrix L01=solveUpper1(iA01,U11);
 
-   			matrix iA01=subtract(A01,multiply(L00,U01));
-			L01= cilk_spawn solveUpper(iA01,U11);
-
-   	
-   	
-   			matrix iA11= subtract(A11,multiply(L10,U01));
-			L11=cilk_spawn solveUpper(iA11,U11);
-  	cilk_sync;
-
+	
+	matrix iA11=subtract(A11,multiply(L10,U01));
+	matrix L11=solveUpper1(iA11,U11);
 	
 	return combineMatrix(L00,L01,L10,L11);
 }
-matrix solveLower(matrix A, matrix L){
+matrix solveLower1(matrix A, matrix L){
 	
 	if(A.row==0 || A.col==0){
 	
@@ -190,28 +160,106 @@ matrix solveLower(matrix A, matrix L){
 	
 	
 	matrix U00(1,1);
-	U00.set(A.mat,0,0);
+	U00.set(A,0,0);
 	
 	matrix U01(1,A.col-1);
-	U01.set(A.mat,0,1);
+	U01.set(A,0,1);
 	
 	matrix A10(A.row-1,1),A11(A.row-1,A.col-1);
-	A10.set(A.mat,1,0);
-	A11.set(A.mat,1,1);
+	A10.set(A,1,0);
+	A11.set(A,1,1);
 	
 	matrix L10(L.row-1,1),L11(L.row-1,L.col-1);
-	L10.set(L.mat,1,0);
-	L11.set(L.mat,1,1);
+	L10.set(L,1,0);
+	L11.set(L,1,1);
 	
 	matrix iA10=subtract(A10,multiply(L10,U00));
-	matrix U10=cilk_spawn solveLower(iA10,L11);
+	matrix U10=solveLower1(iA10,L11);
 	
 	matrix iA11=subtract(A11,multiply(L10,U01));
-	matrix U11=cilk_spawn solveLower(iA11,L11);
-	cilk_sync;
+	matrix U11=solveLower1(iA11,L11);
 	
 	return combineMatrix(U00,U01,U10,U11);
 }
+matrix solveLower(matrix A, matrix L){ //div/2 algo 
+	
+	if(A.row<16)
+		return solveLower1(A,L);
+	//printf("r %d, C=%d A\n",A.row,A.col);
+	int r=A.row;
+	int c=A.col;
+	
+	int r1=r/2; 
+	int r2=r-r1;
+	int c1=c/2;
+	int c2=c-c1;
+	
+	matrix A00(r1,c1),A01(r1,c2),A10(r2,c1),A11(r2,c2);
+	A00.set(A,0,0);
+	A01.set(A,0,c1);
+	A10.set(A,r1,0);
+	A11.set(A,r1,c1);
+	
+	int cL1=L.col/2;
+	int cL2=L.col-cL1; 
+	matrix L00(r1,cL1),L10(r2,cL1),L11(r2,cL2);
+	L00.set(L,0,0);
+	L10.set(L,r1,0);
+	L11.set(L,r1,cL1);
+	
+	matrix U00=solveLower(A00,L00);
+	matrix U01=solveLower(A01,L00);
+	
+	matrix iA10=subtract(A10,multiply(L10,U00));
+	matrix U10=solveLower(iA10,L11); 
+	
+	matrix iA11=subtract(A11,multiply(L10,U01));
+	matrix U11=solveLower(iA11,L11); 
+	
+	
+	return combineMatrix(U00,U01,U10,U11);
+}
+
+matrix solveUpper(matrix A, matrix U){// div/2 algo 
+	
+	if(A.row<16)
+		return solveUpper1(A,U);
+	int r=A.row;
+	int c=A.col;
+	
+	int r1=r/2; 
+	int r2=r-r1;
+	int c1=c/2;
+	int c2=c-c1;
+	
+	matrix A00(r1,c1),A01(r1,c2),A10(r2,c1),A11(r2,c2);
+	A00.set(A,0,0);
+	A01.set(A,0,c1);
+	A10.set(A,r1,0);
+	A11.set(A,r1,c1);
+	
+	int rU1=U.row/2;
+	int rU2=U.row-rU1;
+	matrix U00(rU1,c1),U01(rU1,c2),U11(rU2,c2);
+	U00.set(U,0,0);
+	U01.set(U,0,c1);
+	U11.set(U,rU1,c1);
+	
+	matrix L00=cilk_spawn solveUpper(A00,U00);
+	matrix L10=cilk_spawn solveUpper(A10,U00);
+	cilk_sync;
+	
+	matrix iA01=cilk_spawn subtract(A01,multiply(L00,U01));
+	matrix iA11=cilk_spawn subtract(A11,multiply(L10,U01));
+	cilk_sync;
+	
+	matrix L01=cilk_spawn solveUpper(iA01,U11); 
+	matrix L11=cilk_spawn solveUpper(iA11,U11); 
+	cilk_sync;
+	
+	return combineMatrix(L00,L01,L10,L11);
+}
+
 
 matrix makeL(matrix L00,matrix L10,matrix L11){
 
@@ -273,16 +321,16 @@ pair<matrix,matrix> LUDecompose(matrix A){
 	int c1=c/2;
 	int c2=c-c1;
 	matrix A00(r1,c1);
-	A00.set(A.mat,0,0);
+	A00.set(A,0,0);
 	
 	matrix A01(r1,c2);
-	A01.set(A.mat,0,c1);
+	A01.set(A,0,c1);
 	
 	matrix A10(r2,c1);
-	A10.set(A.mat,r1,0);
+	A10.set(A,r1,0);
 	
 	matrix A11(r2,c2);
-	A11.set(A.mat,r1,c1);
+	A11.set(A,r1,c1);
 	
 	
 	matrix L,U;
@@ -313,14 +361,7 @@ pair<matrix,matrix> LUDecompose(matrix A){
 	return pair<matrix,matrix>(L,U);
 	
 }
-void getMatrix(int dim,double A[]){
 
-	freopen("A.txt","r",stdin);
-	for(int i=0;i<dim;i++)
-		for(int j=0;j<dim;j++)
-			cin>>A[i*dim+j];
-
-}
 string isEqual(matrix A,matrix B){//auxiliary method
 	string False("False"),True("True");
 	for(int i=0;i<A.row;i++)
@@ -329,9 +370,17 @@ string isEqual(matrix A,matrix B){//auxiliary method
 				return False;
 	return True;
 }
+void getMatrix(int dim,double A[]){
+
+	freopen("A2500.txt","r",stdin);
+	for(int i=0;i<dim;i++)
+		for(int j=0;j<dim;j++)
+			cin>>A[i*dim+j];
+
+}
 int main(){
 	
-	const int dim=500;
+	const int dim=2500;
 	
 	double ar[dim*dim];
 	getMatrix(dim,ar);
@@ -340,19 +389,12 @@ int main(){
 	AA.set(ar);
 	pair<matrix, matrix> p;
 	
-	
+	__cilkrts_set_param("nworkers","2");
 	
 	double start_t=omp_get_wtime();
 	
 			p=LUDecompose(AA);
-		
-			
-			
-			/*
-			p.first.show();
-			p.second.show();
-			multiply(p.first,p.second).show();
-			*/
+
 	double elapse_t=omp_get_wtime()-start_t;
 
 	
